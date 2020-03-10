d72b94f62e17543672c85955424a5b56d052b804
ShankarenfoPDharma <Shankarenfo@gmail.com>
2020-03-10 14:18:25 -0700

    sql insert data updated

--- ClothsShopApp/src/main/resources/db/sql/insert-data.sql
+++ ClothsShopApp/src/main/resources/db/sql/insert-data.sql
@@ -1,3 +1,7 @@
-INSERT INTO users(email,username,password) VALUES('lis@douglascollege.ca', 'Simon Li', '123456');
-INSERT INTO users(email,username,password) VALUES('wongi5@douglascollege.ca', 'Ivan Wong', '654321');
-INSERT INTO users(email,username,password,admin) VALUES('AdminTest', 'Test Mckaine', '111111','1');
+insert into users (id, email, username, password, admin) values (1, 'cgellan0@free.fr', 'Cherye Gellan', 'oK75gG', 0);
+insert into users (id, email, username, password, admin) values (2, 'rberkely1@unesco.org', 'Raviv Berkely', 'gzJmsue4DW', 1);
+insert into users (id, email, username, password, admin) values (3, 'vizzard2@vistaprint.com', 'Vicki Izzard', '6DRAnAv', 1);
+insert into users (id, email, username, password, admin) values (4, 'lbambrough3@sina.com.cn', 'Lissi Bambrough', 'FiRGuMX', 1);
+insert into users (id, email, username, password, admin) values (5, 'ncraft4@ft.com', 'Nisse Craft', 'xUsSU8F', 0);
+insert into users (id, email, username, password, admin) values (6, 'pkinnock5@constantcontact.com', 'Pail Kinnock', 'inxPj9XU0d', 0);
+
